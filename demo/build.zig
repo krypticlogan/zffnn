71ffaf5c97812f966227a89e5f80dfe7670eecf6
= <=>
2026-03-07 16:13:57 -0700

    demo of the lib using raylib

---
+++ demo/build.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const nn_dep = b.dependency("zig_nn", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const zffnn = nn_dep.module("zig_nn");
+
+    const raylib_dep = b.dependency("raylib_zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib"); // main raylib module
+    const raygui = raylib_dep.module("raygui"); // raygui module
+    const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
+
+    const exe = b.addExecutable(.{
+        .name = "demo",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "raylib", .module = raylib },
+                .{ .name = "raygui", .module = raygui },
+                .{ .name = "zffnn", .module = zffnn },
+            },
+            .link_libc = true,
+        }),
+    });
+    exe.root_module.linkLibrary(raylib_artifact);
+
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the app");
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+}
