8fe2a335e559ceafe53f19f3121434d47ac26e82
Logan <logan.zchjones@gmail.com>
2026-03-13 04:37:24 -0700

    receive optional param directory

--- build.zig
+++ build.zig
@@ -3,12 +3,14 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseSmall });
-
+    const options = b.addOptions();
+    options.addOption(?std.Build.LazyPath, "params", null);
     const mod = b.addModule("zffnn", .{
         .root_source_file = b.path("src/ffnn.zig"),
         .target = target,
-        .optimize=optimize
+        .optimize= optimize
     });
+    mod.addOptions("build_options", options);
 
     const mod_tests = b.addTest(.{
         .root_module = mod,
