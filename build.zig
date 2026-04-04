fb05270b8fea30dc438ad0a62774ccce0d22cf2f
Logan <logan.zchjones@gmail.com>
2026-04-04 14:11:59 -0700

    more ergonomic parameter embedding

--- build.zig
+++ build.zig
@@ -4,11 +4,22 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     // library
-    const mod = b.addModule("zffnn", .{
-        .root_source_file = b.path("src/ffnn.zig"),
+    const mod = b.addModule("zffnn", .{ 
+        .root_source_file = b.path("src/root.zig"), 
         .target = target,
-        .optimize= optimize
+        .optimize = optimize,
     });
+    
+    // embed params generator
+    const gen = b.addExecutable(.{
+        .name = "embed_helper",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("embed_helper/embeddings_gen.zig"),
+            .target = b.graph.host,
+            .optimize = optimize,
+        }),
+    });
+    b.installArtifact(gen);
 
     // tests
     const mod_tests = b.addTest(.{
@@ -16,6 +27,6 @@ pub fn build(b: *std.Build) void {
     });
     const run_mod_tests = b.addRunArtifact(mod_tests);
     const test_step = b.step("test", "Run tests");
-    
+
     test_step.dependOn(&run_mod_tests.step);
 }
--- demo/build.zig
+++ demo/build.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -10,12 +9,27 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     const zffnn = nn_dep.module("zffnn");
-    addEmbeddedParams(
-        b, 
-        zffnn, 
-        b.path("model_params"), 
-        3
-    );
+    
+    { // Run the embed helper to generate the embeds.zig file
+        const params_dir = b.path("model_params");
+        const embed_file_name = "embeds.zig";
+        
+        const zffn_embed_gen = nn_dep.artifact("embed_helper");
+        const run_gen = b.addRunArtifact(zffn_embed_gen);
+        run_gen.addArg("3"); // number of trainable layers
+        run_gen.addArg(embed_file_name);
+        run_gen.addDirectoryArg(params_dir); // directory containing model parameters
+        
+        const out_dir = run_gen.addOutputDirectoryArg("zffnn_embeds"); // output directory for the generated embeds.zig file
+
+        const embed_mod = b.createModule(.{
+            .root_source_file = out_dir.path(b, embed_file_name),
+            .target = target,
+            .optimize = optimize,
+        });
+        // add the embed module to the zffnn import, the name "embed_params" must be used
+        zffnn.addImport("embed_params", embed_mod);
+    }
 
     const raylib_dep = b.dependency("raylib_zig", .{
         .target = target,
@@ -55,37 +69,37 @@ pub fn build(b: *std.Build) void {
     }
 }
 
-pub fn addEmbeddedParams(
-    b: *std.Build,
-    target_mod: *std.Build.Module,
-    /// - The files should be named 'w{layer_index}.bin' and 'b{layer_index}.bin' for each layer.
-    dir: std.Build.LazyPath,
-    /// Number of trainable layers.
-    layer_count: usize,
-) void {
-    const wf = b.addWriteFiles();
+// pub fn addEmbeddedParams(
+//     b: *std.Build,
+//     target_mod: *std.Build.Module,
+//     /// - The files should be named 'w{layer_index}.bin' and 'b{layer_index}.bin' for each layer.
+//     dir: std.Build.LazyPath,
+//     /// Number of trainable layers.
+//     layer_count: usize,
+// ) void {
+//     const wf = b.addWriteFiles();
 
-    var zig_src: std.ArrayList(u8) = .empty;
-    defer zig_src.deinit(b.allocator);
-    
-    zig_src.writer(b.allocator).writeAll("pub const weights = [_][]const u8{\n") catch unreachable;
-    for (1..layer_count + 1) |i| {
-        const w_src = dir.path(b, b.fmt("w{d}.bin", .{i}));
-        _ = wf.addCopyFile(w_src, b.fmt("w{d}.bin", .{i}));
-        zig_src.writer(b.allocator).print("    @embedFile(\"w{d}.bin\"),\n", .{i}) catch unreachable;
-    }
-    zig_src.writer(b.allocator).writeAll("};\n\n") catch unreachable;
+//     var zig_src: std.ArrayList(u8) = .empty;
+//     defer zig_src.deinit(b.allocator);
 
-    zig_src.writer(b.allocator).writeAll("pub const biases = [_][]const u8{\n") catch unreachable;
-    for (1..layer_count + 1) |i| {
-        const b_src = dir.path(b, b.fmt("b{d}.bin", .{i}));
-        _ = wf.addCopyFile(b_src, b.fmt("b{d}.bin", .{i}));
-        zig_src.writer(b.allocator).print("    @embedFile(\"b{d}.bin\"),\n", .{i}) catch unreachable;
-    }
-    zig_src.writer(b.allocator).writeAll("};\n") catch unreachable;
+//     zig_src.writer(b.allocator).writeAll("pub const weights = [_][]const u8{\n") catch unreachable;
+//     for (1..layer_count + 1) |i| {
+//         const w_src = dir.path(b, b.fmt("w{d}.bin", .{i}));
+//         _ = wf.addCopyFile(w_src, b.fmt("w{d}.bin", .{i}));
+//         zig_src.writer(b.allocator).print("    @embedFile(\"w{d}.bin\"),\n", .{i}) catch unreachable;
+//     }
+//     zig_src.writer(b.allocator).writeAll("};\n\n") catch unreachable;
 
-    const embeds = wf.add("zffnn_embeds.zig", zig_src.items);
-    target_mod.addAnonymousImport("embed_params", .{
-        .root_source_file = embeds,
-    });
-}
+//     zig_src.writer(b.allocator).writeAll("pub const biases = [_][]const u8{\n") catch unreachable;
+//     for (1..layer_count + 1) |i| {
+//         const b_src = dir.path(b, b.fmt("b{d}.bin", .{i}));
+//         _ = wf.addCopyFile(b_src, b.fmt("b{d}.bin", .{i}));
+//         zig_src.writer(b.allocator).print("    @embedFile(\"b{d}.bin\"),\n", .{i}) catch unreachable;
+//     }
+//     zig_src.writer(b.allocator).writeAll("};\n") catch unreachable;
+
+//     const embeds = wf.add("zffnn_embeds.zig", zig_src.items);
+//     target_mod.addAnonymousImport("embed_params", .{
+//         .root_source_file = embeds,
+//     });
+// }
