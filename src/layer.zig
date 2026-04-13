089adc92ebab6a239d6b35d4923a12ca357a125c
Logan <logan.zchjones@gmail.com>
2026-04-13 02:43:50 -0700

    new classifaction and kernel decisions

--- src/layer.zig
+++ src/layer.zig
@@ -4,11 +4,42 @@ const Activation = @import("activations.zig").Activation;
 
 pub const layer_kind = union(enum) { input, hidden, output };
 
+const Role = enum {
+    input,
+    hidden,
+    output,
+    // softmax/output layers?
+};
+
+const BatchStandard = enum {
+    one, // this is for models of batch size 1, they're super fast and get a special path
+    multi, // this is the general path for moderately sized models with batched outputs
+};
+
+const medium_model_threshold = 32 * 1024; // b
+const large_model_threshold = 512 * 1024; // b
+const SizeStandard = enum {
+    small, // this is for small models
+    medium, // this is for medium models
+    large, // this is for large models
+};
+
+const ShapeStandard = enum {
+  wide,
+  skinny,
+  square,
+};
+
 pub fn Layer(kind: layer_kind, activation: Activation, comptime len: usize, comptime parent_len: usize, batch_size: usize) type {
-    const batched = batch_size > 1;
     return struct {
+        // first, we do some classification of the layer's shape and size to determine the best path
+        const active_bytes = @sizeOf(f32) * (parent_len * len + batch_size * (parent_len + len)); // sizeof dtype ( out * in + batch_size * (out + in) );
+        const size_standard: SizeStandard = if (active_bytes > large_model_threshold) .large else if (active_bytes > medium_model_threshold) .medium else .small;
+        const batch_standard: BatchStandard = if (batch_size == 1) .one else .multi;
+        const shape_standard: ShapeStandard = if (len > parent_len) .wide else if (len < parent_len) .skinny else .square;
+        
+
         weights: Mat(len, parent_len) = undefined,
-        z: Mat(len, batch_size) = undefined,
         a: Mat(len, batch_size) = undefined,
         bias: Mat(len, 1) = undefined,
         activation: Activation = undefined,
@@ -20,15 +51,15 @@ pub fn Layer(kind: layer_kind, activation: Activation, comptime len: usize, comp
         pub fn init(layer: *@This()) void {
             layer.activation = activation;
             layer.kind = kind;
+            const desc = std.fmt.comptimePrint("Layer.init: kind={any} layer_sz_bytes={any}b regime={any} batch_standard={any}\n", .{layer.kind, active_bytes, size_standard, batch_standard});
+            @compileLog(desc);
             switch (layer.kind) {
                 .hidden, .output => {
                     layer.bias.init(0);
                     layer.weights.init(0);
-                    layer.z.init(0);
                     layer.a.init(0);
                 },
                 .input => {
-                    layer.z.init(0);
                     layer.a.init(0);
                 },
             }
@@ -40,10 +71,31 @@ pub fn Layer(kind: layer_kind, activation: Activation, comptime len: usize, comp
             self.weights.random_fill(prng.random());
         }
 
-        pub fn forward(self: *@This(), layer_input: Mat(parent_len, batch_size)) void {
+        pub fn forward(self: *@This(), layer_input: *const Mat(parent_len, batch_size)) void {
             if (self.kind == .input) @panic("Do not pass over the input layer. Complete all normalization and preprocessing prior to forward feed");
-            self.z = self.weights.mul(layer_input, batched).add(self.bias);
-            self.a = self.activation.apply(self.z, batched);
+            if (batch_standard == .one) {
+                // std.debug.print("fast", .{});
+                self.a = self.weights.mul(layer_input, false).add(self.bias);
+                self.activation.apply(&self.a, false);
+            } else {
+                switch (size_standard) {
+                    .small => {
+                        self.a = self.weights.mul(layer_input, true).add(self.bias);
+                        self.activation.apply(&self.a, true);
+                    },
+                    .medium => {
+                        self.weights.mul_into(layer_input, &self.a, true, false);
+                        self.a.i_add(self.bias);
+                        self.activation.apply(&self.a, true);
+                    },
+                    .large => {
+                        // std.debug.print("large", .{});
+                        self.weights.mul_into(layer_input, &self.a, true, true);
+                        self.a.i_add(self.bias);
+                        self.activation.apply(&self.a, true);
+                    },
+                }
+            }
         }
     };
 }
