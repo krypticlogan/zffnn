60b1771dddff9496663fad06d4704753cbf583fd
Logan <logan.zchjones@gmail.com>
2026-04-13 02:44:23 -0700

    new ops for specific kernels, new Row and Col types for easy access

--- src/matrix.zig
+++ src/matrix.zig
@@ -23,8 +23,18 @@ pub fn Mat(comptime row_ct: usize, comptime col_ct: usize) type {
         pub const n = row_ct;
         pub const m = col_ct;
         const Axis = union(enum) { r, c };
+        const Row = @Vector(m, f32);
+        const Col = @Vector(n, f32);
 
-        data: [n]@Vector(m, f32), // row-major [2, 4] is 2 rows x 4 columns
+        const tile_width = std.simd.suggestVectorLength(f32) orelse 1;
+        const TiledRow = @Vector(tile_width, f32);
+        const row_tiles = m / tile_width;
+        const row_tiles_leftover = m % tile_width;
+
+        const col_tiles = n / tile_width;
+        const col_tiles_leftover = n % tile_width;
+
+        data: [n]Row,
 
         pub fn init(mat: *This, fill_with: f32) void {
             mat.fill(fill_with);
@@ -77,13 +87,13 @@ pub fn Mat(comptime row_ct: usize, comptime col_ct: usize) type {
             return m;
         }
 
-        pub inline fn r(self: *const This, comptime i: usize) @Vector(n, f32) {
+        pub inline fn r(self: *const This, comptime i: usize) Row {
             return self.data[i];
         }
 
-        pub inline fn c(self: *const This, comptime i: usize) @Vector(m, f32) { // todo: come back to this
-            var col: @Vector(m, f32) = undefined;
-            inline for (0..m) |j| {
+        pub inline fn c(self: *const This, comptime i: usize) Col { // todo: come back to this
+            var col: Col = undefined;
+            inline for (0..n) |j| {
                 col[j] = self.data[j][i];
             }
             return col;
@@ -113,8 +123,7 @@ pub fn Mat(comptime row_ct: usize, comptime col_ct: usize) type {
         }
 
         pub fn clear(mat: *This) void {
-            mat.* = undefined;
-            mat.* = This.create(0);
+            mat.fill(0);
         }
 
         pub inline fn max_vec(vec: anytype) f32 {
@@ -163,7 +172,7 @@ pub fn Mat(comptime row_ct: usize, comptime col_ct: usize) type {
 
         pub fn exp(mat: *const This) This {
             var out = Mat(n, m).create(0);
-            for (0..n) |row| {
+            for (0..mat.rows()) |row| {
                 out.data[row] = exp_row(mat.data[row]);
             }
             return out;
@@ -188,11 +197,24 @@ pub fn Mat(comptime row_ct: usize, comptime col_ct: usize) type {
                 },
                 .per_row => for (0..out.rows()) |row| { // simd per row due to vector type
                     const bi: f32 = b.data[row][0];
-                    out.data[row] = a.data[row] + @as(@Vector(m, f32), @splat(bi));
+                    out.data[row] = a.data[row] + @as(Row, @splat(bi));
                 },
                 .none => @compileError("Your add is misaligned, A and B must have matching rows!"),
             }
             return out;
+        }
+
+        pub fn i_add(a: *This, b: anytype) void {
+            switch (comptime add_is_defined(@TypeOf(a.*), @TypeOf(b))) {
+                .full => for (0..a.rows()) |row| { // simd per row due to vector type
+                    a.data[row] += b.data[row];
+                },
+                .per_row => for (0..a.rows()) |row| { // simd per row due to vector type
+                    const bi: f32 = b.data[row][0];
+                    a.data[row] += @as(Row, @splat(bi));
+                },
+                .none => @compileError("Your add is misaligned, A and B must have matching rows!"),
+            }
         }
 
         pub fn sub(a: *const This, b: anytype) This {
@@ -203,11 +225,24 @@ pub fn Mat(comptime row_ct: usize, comptime col_ct: usize) type {
                 },
                 .per_row => for (0..out.rows()) |row| { // simd per row due to vector type
                     const bi: f32 = b.data[row][0];
-                    out.data[row] = a.data[row] - @as(@Vector(m, f32), @splat(bi));
+                    out.data[row] = a.data[row] - @as(Row, @splat(bi));
                 },
                 .none => @compileError("Your sub is misaligned, A and B must have matching rows!"),
             }
             return out;
+        }
+
+        pub fn i_sub(a: *This, b: anytype) void {
+            switch (comptime add_is_defined(@TypeOf(a.*), @TypeOf(b))) {
+                .full => for (0..a.rows()) |row| { // simd per row due to vector type
+                    a.data[row] -= b.data[row];
+                },
+                .per_row => for (0..a.rows()) |row| { // simd per row due to vector type
+                    const bi: f32 = b.data[row][0];
+                    a.data[row] -= @as(Row, @splat(bi));
+                },
+                .none => @compileError("Your sub is misaligned, A and B must have matching rows!"),
+            }
         }
 
         fn add_is_defined(comptime a_type: anytype, comptime b_type: anytype) union(enum) { none, full, per_row } {
@@ -227,9 +262,9 @@ pub fn Mat(comptime row_ct: usize, comptime col_ct: usize) type {
             return @reduce(.Add, a * b);
         }
 
-        pub fn mul(a: *const This, b: anytype, batched: bool) Mat(n, @TypeOf(b).m) {
-            if (!comptime is_matrix(@TypeOf(a.*)) or !is_matrix(@TypeOf(b))) @compileError("The 'matrix' you provided is not really a matrix");
-            if (!comptime mul_is_defined(@TypeOf(a.*), @TypeOf(b))) @compileError("Your multipication is misaligned, B must have the same number of rows as A has columns!");
+        pub fn mul(a: *const This, b: anytype, batched: bool) Mat(n, @TypeOf(b.*).m) { // fast path
+            if (!comptime is_matrix(@TypeOf(a.*)) or !is_matrix(@TypeOf(b.*))) @compileError("The 'matrix' you provided is not really a matrix");
+            if (!comptime mul_is_defined(@TypeOf(a.*), @TypeOf(b.*))) @compileError("Your multipication is misaligned, B must have the same number of rows as A has columns!");
             if (batched) {
                 return batch_mul(a, b);
             } else {
@@ -237,8 +272,8 @@ pub fn Mat(comptime row_ct: usize, comptime col_ct: usize) type {
             }
         }
 
-        fn batch_mul(a: *const This, b: anytype) Mat(n, @TypeOf(b).m) {
-            var out = Mat(n, @TypeOf(b).m).create(0);
+        fn batch_mul(a: *const This, b: anytype) Mat(n, @TypeOf(b.*).m) {
+            var out = Mat(n, @TypeOf(b.*).m).create(0);
             for (0..n) |row| {
                 for (0..m) |col| { // broadcasts the row of A to each column of B and sums their product to the output
                     out.data[row] += @as(@TypeOf(out.data[row]), @splat(a.data[row][col])) * b.data[col];
@@ -247,20 +282,69 @@ pub fn Mat(comptime row_ct: usize, comptime col_ct: usize) type {
             return out;
         }
 
-        fn single_mul(a: *const This, b: anytype) Mat(n, @TypeOf(b).m) {
-            var out = Mat(n, @TypeOf(b).m).create(0);
+        fn single_mul(a: *const This, b: anytype) Mat(n, @TypeOf(b.*).m) { // fast path todo: room for improvement probably
+            var out = Mat(n, @TypeOf(b.*).m).create(0);
             const b_t = b.t();
-
             for (0..n) |row| {
-                var out_row: @Vector(@TypeOf(b).m, f32) = undefined;
-                for (0..@TypeOf(b).m) |col| {
+                var out_row: @TypeOf(b.*).Row = undefined;
+                for (0..@TypeOf(b.*).m) |col| {
                     out_row[col] = dot(m, a.data[row], b_t.data[col]);
                 }
                 out.data[row] = out_row;
             }
-
             return out;
         }
+
+        pub fn mul_into(a: *const This, b: anytype, out: *Mat(n, @TypeOf(b.*).m), batched: bool, large: bool) void {
+            if (!comptime is_matrix(@TypeOf(a.*)) or !is_matrix(@TypeOf(b.*))) @compileError("The 'matrix' you provided is not really a matrix");
+            if (!comptime mul_is_defined(@TypeOf(a.*), @TypeOf(b.*))) @compileError("Your multipication is misaligned, B must have the same number of rows as A has columns!");
+            if (batched) {
+                if (large) {
+                    large_mul_into(a, b, out);
+                } else {
+                    batch_mul_into(a, b, out);
+                }
+            } else {
+                single_mul_into(a, b, out);
+            }
+        }
+        
+        pub fn large_mul_into(a: *const This, b: anytype, out: *Mat(n, @TypeOf(b.*).m)) void {
+            out.clear();
+            // std.debug.print("large_mul_into: n={} m={}\n", .{n, m});
+            const R = @TypeOf(a.*).tile_width;
+            var _r: usize = 0;
+            while (_r < n) : (_r += R) {
+                const r_end = @min(_r + R, n);
+                var k: usize = 0;
+                while (k < m) : (k += 1) {
+                    var rr = _r;
+                    while (rr < r_end) : (rr += 1) {
+                        out.data[rr] += @as(@TypeOf(b.*).Row, @splat(a.data[rr][k])) *  b.data[k];
+                    }
+                }
+            }
+        }
+
+        pub fn batch_mul_into(a: *const This, b: anytype, out: *Mat(n, @TypeOf(b.*).m)) void {
+            out.clear();
+            for (0..n) |row| {
+                for (0..m) |col| { // broadcasts the row of A to each column of B and sums their product to the output
+                    out.data[row] += @as(@TypeOf(b.*).Row, @splat(a.data[row][col])) * b.data[col];
+                }
+            }
+        }
+
+        pub fn single_mul_into(a: *const This, b: anytype, out: *Mat(n, @TypeOf(b.*).m)) void {
+            out.clear();
+            const b_t = b.t();
+            for (0..n) |row| {
+                for (0..@TypeOf(b.*).m) |col| {
+                    out.data[row][col] += dot(m, a.data[row], b_t.data[col]);
+                }
+            }
+        }
+        
 
         fn mul_is_defined(comptime a_type: anytype, comptime b_type: anytype) bool {
             return a_type.m == b_type.n;
