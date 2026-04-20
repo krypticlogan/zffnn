432c940770e85cfe0fa2f44d273118aaeabe949c
logan <logan.zchjones@gmail.com>
2026-04-20 12:01:34 -0700

    io and file writing changes

--- embed_helper/embedding.zig
+++ embed_helper/embedding.zig
@@ -1,69 +1,75 @@
 const std = @import("std");
 
 pub fn writeEmbeddedParamsBundle(
+    io: std.Io,
     allocator: std.mem.Allocator,
     embed_file_name: []const u8,
     input_dir_path: []const u8,
     output_dir_path: []const u8,
     layer_count: usize,
 ) !void {
-    const cwd = std.fs.cwd();
+    const cwd = std.Io.Dir.cwd();
 
-    try cwd.makePath(output_dir_path);
+    try cwd.createDirPath(io, output_dir_path);
 
-    var input_dir = try cwd.openDir(input_dir_path, .{});
-    defer input_dir.close();
+    var input_dir = try cwd.openDir(io, input_dir_path, .{});
+    defer input_dir.close(io);
 
-    var output_dir = try cwd.openDir(output_dir_path, .{});
-    defer output_dir.close();
+    var output_dir = try cwd.openDir(io, output_dir_path, .{});
+    defer output_dir.close(io);
 
     var zig_src: std.ArrayList(u8) = .empty;
     defer zig_src.deinit(allocator);
 
-    const w = zig_src.writer(allocator);
+    // const w = zig_src.writer(allocator);
 
-    try w.writeAll("pub const weights = [_][]const u8{\n");
+    try zig_src.appendSlice(allocator, "pub const weights = [_][]const u8{\n");
     for (1..layer_count + 1) |i| {
         const src_name = try std.fmt.allocPrint(allocator, "w{d}.bin", .{i});
         defer allocator.free(src_name);
-
-        try copyFileIntoDir(input_dir, output_dir, src_name);
-        try w.print("    @embedFile(\"w{d}.bin\"),\n", .{i});
+        
+        const line = try std.fmt.allocPrint(allocator, "    @embedFile(\"w{d}.bin\"),\n", .{i});
+        defer allocator.free(line);
+        try copyFileIntoDir(io, input_dir, output_dir, src_name);
+        try zig_src.appendSlice(allocator, line);
     }
-    try w.writeAll("};\n\n");
+    try zig_src.appendSlice(allocator, "};\n\n");
 
-    try w.writeAll("pub const biases = [_][]const u8{\n");
+    try zig_src.appendSlice(allocator, "pub const biases = [_][]const u8{\n");
     for (1..layer_count + 1) |i| {
         const src_name = try std.fmt.allocPrint(allocator, "b{d}.bin", .{i});
         defer allocator.free(src_name);
-
-        try copyFileIntoDir(input_dir, output_dir, src_name);
-        try w.print("    @embedFile(\"b{d}.bin\"),\n", .{i});
+        
+        const line = try std.fmt.allocPrint(allocator, "    @embedFile(\"b{d}.bin\"),\n", .{i});
+        try copyFileIntoDir(io, input_dir, output_dir, src_name);
+        try zig_src.appendSlice(allocator, line);
     }
-    try w.writeAll("};\n");
+    try zig_src.appendSlice(allocator, "};\n");
 
-    try output_dir.writeFile(.{
+    try output_dir.writeFile(io,
+        .{
         .sub_path = embed_file_name,
         .data = zig_src.items,
     });
 }
 
 fn copyFileIntoDir(
-    src_dir: std.fs.Dir,
-    dst_dir: std.fs.Dir,
+    io: std.Io,
+    src_dir: std.Io.Dir,
+    dst_dir: std.Io.Dir,
     name: []const u8,
 ) !void {
-    var src_file = try src_dir.openFile(name, .{});
-    defer src_file.close();
+    var src_file = try src_dir.openFile(io, name, .{});
+    defer src_file.close(io);
 
-    var dst_file = try dst_dir.createFile(name, .{ .truncate = true });
-    defer dst_file.close();
+    var dst_file = try dst_dir.createFile(io, name, .{ .truncate = true });
+    defer dst_file.close(io);
 
     var rbuf: [4096]u8 = undefined;
     var wbuf: [4096]u8 = undefined;
 
-    var r = src_file.reader(&rbuf);
-    var w = dst_file.writer(&wbuf);
+    var r = src_file.reader(io, &rbuf);
+    var w = dst_file.writer(io, &wbuf);
 
     _ = try r.interface.streamRemaining(&w.interface);
     try w.interface.flush();
--- embed_helper/embeddings_gen.zig
+++ embed_helper/embeddings_gen.zig
@@ -1,14 +1,9 @@
 const std = @import("std");
 const codegen = @import("embedding.zig");
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-
-    var args = try std.process.argsWithAllocator(allocator);
-    defer args.deinit();
-
+pub fn main(init: std.process.Init) !void {
+    const gpa = init.gpa;
+    var args = try init.minimal.args.iterateAllocator(gpa);
     _ = args.skip(); // argv[0]
 
     const layer_count_str = args.next() orelse return error.MissingLayerCount;
@@ -19,7 +14,8 @@ pub fn main() !void {
     const layer_count = try std.fmt.parseInt(usize, layer_count_str, 10);
 
     try codegen.writeEmbeddedParamsBundle(
-        allocator,
+        init.io,
+        gpa,
         embed_file_name,
         input_dir,
         output_dir,
