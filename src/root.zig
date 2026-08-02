1d3ea95e348089966e83f92ad6166f721eaeb646
logan <logan.zchjones@gmail.com>
2026-08-01 17:17:57 -0700

    Fix root.zig

--- src/root.zig
+++ src/root.zig
@@ -8,7 +8,7 @@ pub const Tensor = @import("zgc/tensor.zig");
 pub const CountingBackend = @import("zgc/builder.zig").CapacityCountingBackend;
 pub const GraphBackend = @import("zgc/builder.zig").GraphBackend;
 pub const Model = @import("zgc/model.zig").Model;
-pub const Storage = @import("zgc/storage.zig").Storage;
+pub const Storage = @import("zgc/storage.zig");
 pub const Extensions = @import("extensions/ext.zig");
 
 // pub const network_validation = @import("network_validation.zig");
