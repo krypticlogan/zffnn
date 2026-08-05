eb18048c732f9328275347beef3a78bc56009276
logan <logan.zchjones@gmail.com>
2026-08-05 16:08:02 -0700

    update sandbox model architecture

--- sandbox/src/model.zig
+++ sandbox/src/model.zig
@@ -7,10 +7,11 @@ pub const Sources = enum(usize) {
 };
 
 fn defineGraph(builder: anytype) void {
-    const a = builder.input(Sources.a, .f32, &.{ 2, 3 });
-    const b = builder.input(Sources.a, .f32, &.{ 2, 3 });
-    const c = builder.add(a, b);
-    builder.output(builder.relu(c));
+    const a = builder.input(Sources.a, .f32, &.{ 3, 3 });
+    const b = builder.input(Sources.a, .f32, &.{ 3, 3 });
+    const sum = builder.add(a, b);
+    const product = builder.matmul(sum, a);
+    builder.output(builder.add(product, sum));
 }
 
 pub const capacity = blk: {
@@ -29,8 +30,16 @@ pub const graph = blk: {
 };
 
 pub const Model = zgc.Model(capacity, graph);
-pub const a_values = [_]f32{ -3.5, -0.0, 0.0, 2.25, -1.0, 8.0 };
-pub const b_values = [_]f32{ -3.5, -0.0, 0.0, 2.25, -1.0, 8.0 };
+pub const a_values = [_]f32{ 
+    -3.5, 2.0, 0.8, 
+    2.25, 1.0, 5.2, 
+    2.3, 4.2, 7.0 
+};
+pub const b_values = [_]f32{ -
+    2.5, 13.4, 17.3, 
+    6.6, 1.0, 5.9, 
+    9.3, 4.6, 3.5 
+};
 
 pub fn loadInput(model: *Model) void {
     const a_bytes: [@sizeOf(@TypeOf(a_values))]u8 = @bitCast(a_values);
